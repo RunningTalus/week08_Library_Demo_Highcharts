f062e2f403ae46b5abeb3f40eaa5d51f67f797c6
AndyLampert <lamperta@gmail.com>
2014-05-21 14:19:35 -0600

    set up routes and links

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ app.get('/', function(req, res) {
 	res.render('index');
 });
 
+app.get('/andrew', function(req,res){
+	res.render('andrew');
+});
+
+app.get('/dave', function(req,res){
+	res.render('dave');
+});
+
+app.get('/brian', function(req,res){
+	res.render('brian');
+});
+
 var server = app.listen(6975, function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
